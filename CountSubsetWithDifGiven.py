df8dcd5a2975fa29e533cffb64e8a5ef2d1479a8
goutham45 <gouthamc@buffalo.edu>
2023-10-05 15:56:03 -0400

    Knapsack 0/1 - Count th subset with differrence given

---
+++ CountSubsetWithDifGiven.py
@@ -0,0 +1,29 @@
+def subset(arr,sum,n):
+  t = [[0 for x in range(sum+1)] for x in range(n+1)]
+
+  for i in range(n+1):
+      for j in range(sum+1):
+          if i == 0:
+              t[0][j] = 0
+          if j == 0:
+              t[i][0] = 1
+  for i in range(1,n+1):
+    for j in range(1,sum+1):
+      if arr[i-1]<=j:
+        # print(j - arr[i-1])
+        t[i][j] = (t[i-1][j - arr[i-1]] + t[i-1][j])
+      elif arr[i-1]>j:
+        t[i][j] = t[i-1][j]
+  count = 0
+  for row in t:
+    print(row)
+  return t[n][sum]
+
+
+arr = [1]
+dif =2
+ns = (1+dif) // 2
+sum = ns
+n = len(arr)
+print(subset(arr,sum,n))
+
