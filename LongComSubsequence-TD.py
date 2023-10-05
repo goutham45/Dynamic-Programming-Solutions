5d1a83b7e77092eaca07b36c53d18167e7f5baf7
goutham45 <gouthamc@buffalo.edu>
2023-10-05 16:17:01 -0400

    Longest Common Subsequence - Top down

---
+++ LongComSubsequence-TD.py
@@ -0,0 +1,15 @@
+x = "abcdgsg"
+y = "abcfgdf"
+
+n = len(x)
+m = len(y)
+def lcs(x,y,n,m):
+    t = [[0 for x in range(m+1)] for x in range(n+1)]
+    for i in range(1,n+1):
+        for j in range(1,m+1):
+            if x[i-1] == y[j-1]:
+                t[i][j] = 1+t[i-1][j-1]
+            else:
+                t[i][j] = max(t[i-1][j],t[i][j-1])
+    return t[n][m]
+print(lcs(x,y,n,m))
